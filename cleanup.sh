cccf4de8e8dffa00646a8555e95295befff661d6
Ori Shadmon <oshadmon@gmail.com>
2024-10-08 14:11:04 -0700

    cleanup
    Signed-off-by: Ori Shadmon <oshadmon@gmail.com>

--- cleanup.sh
+++ cleanup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 IDS=$(curl -X GET 45.79.74.39:32049  -H 'command: blockchain get * where company="Open Horizon" bring [*][id] separator=" "')
+
 for ID in ${IDS} ; do
   echo ${ID}
-  curl -X POST 45.79.74.39:32049 -H "command: blockchain drop policy where id=${ID}" -H "User-Agent": AnyLog/1.23"
+  curl -X POST 45.79.74.39:32049 -H "command: blockchain drop policy where id=${ID}" -H "User-Agent: AnyLog/1.23"
 done
